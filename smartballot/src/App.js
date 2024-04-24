a6648670e4b6f808970f58b09e198f6611dab9f7
Viaceslav <v.j.vasiljev@gmail.com>
2024-04-25 02:09:23 +0300

    get balance and show adress

--- smartballot/src/App.js
+++ smartballot/src/App.js
@@ -4,16 +4,17 @@ import "./App.css";
 
 function App() {
   const [firstName, setFirstName] = useState("Viaceslav");
-  const [account, setAccount] = useState();
+  const [account, setAccount] = useState(null);
+  const [network, setNetwork] = useState();
+  const [balance, setBalance] = useState();
 
-  //when app first mount perform an action
   useEffect(() => {
-    async function loadAccounts() {
-      if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
 
+      async function loadAccounts() {
         try {
-          // This will prompt MetaMask to connect and return the accounts
+          // Request account access if needed
           const accounts = await window.ethereum.request({
             method: "eth_requestAccounts",
           });
@@ -21,25 +22,48 @@ function App() {
         } catch (error) {
           console.error("Could not retrieve accounts", error);
         }
-      } else {
-        alert("MetaMask is not installed!");
-        console.log("MetaMask is not installed!");
+      }
+
+      loadAccounts();
+    } else {
+      alert("MetaMask is not installed!");
+      console.log("MetaMask is not installed!");
+    }
+  }, []);
+
+  useEffect(() => {
+    async function loadBalance() {
+      if (account) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          const chainId = await web3.eth.getChainId();
+          const balanceWei = await web3.eth.getBalance(account);
+          console.log("ChainID", chainId);
+          setNetwork(chainId);
+          setBalance(web3.utils.fromWei(balanceWei, "ether")); // Convert balance from Wei to Ether
+        } catch (error) {
+          console.error("Could not retrieve balance", error);
+        }
       }
     }
 
-    loadAccounts();
-  }, []);
+    loadBalance();
+  }, [account]); // This useEffect is dependent on `account` and will run when `account` changes
 
   return (
     <div className="App">
       <header className="App-header">
-        Decentrilized Ballot ({firstName})
+        Decentralized Ballot ({firstName})
         <input
           type="text"
           value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
-        ></input>
+        />
         <p>Your Account: {account} </p>
+        <p>
+          Your Balance on Network ID ({network}):{" "}
+          {balance ? `${balance} ETH` : "Loading..."}
+        </p>
       </header>
     </div>
   );
