c432510307b85d0f35afb273cc36ba07ca1ee322
Viaceslav <v.j.vasiljev@gmail.com>
2024-04-26 00:49:06 +0300

    Added succesful transactions, days month weeks

--- smartballot/src/App.js
+++ smartballot/src/App.js
@@ -16,9 +16,19 @@ function App() {
 
   const [transactions, setTransactions] = useState([]);
   const [numTotalTransactions, setNumTotalTransactions] = useState(0);
+  const [numSuccesfulTransactions, setNumSuccesfulTransactions] = useState(0);
   const [ethPrice, setEthPrice] = useState({ BTC: null, USD: null, EUR: null });
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [uniqueDays, setUniqueDays] = useState(new Set());
+  const [uniqueWeeks, setUniqueWeeks] = useState(new Set());
+  const [uniqueMonths, setUniqueMonths] = useState(new Set());
+  const [totalUniqueDays, setTotalUniqueDays] = useState();
+  const [totalUniqueWeeks, setTotalUniqueWeeks] = useState();
+  const [totalUniqueMonths, setTotalUniqueMonths] = useState();
+
+  // Define a state variable to track if a request is currently being processed
+  const [isRequestingAccounts, setIsRequestingAccounts] = useState(false);
 
   //getting the balance from scroll api
   const getEtherBalance = async () => {
@@ -89,6 +99,62 @@ function App() {
       // You can set the transactions data to a state or return it depending on your app's design
       setTransactions(data.result);
       setNumTotalTransactions(data.result.length);
+
+      // Function to calculate the number of successful transactions
+      const calculateSuccessfulTransactions = (transactions) => {
+        return transactions.filter((tx) => tx.isError === "0").length;
+      };
+
+      // Number of successful transactions
+      const numOfSuccessfulTransactions = calculateSuccessfulTransactions(
+        data.result
+      );
+      setNumSuccesfulTransactions(numOfSuccessfulTransactions);
+
+      // Helper function to format a date to YYYY-MM-DD
+      const formatDate = (date) => {
+        return date.toISOString().split("T")[0];
+      };
+
+      // Helper function to get the week number in the year
+      const getWeekNumber = (date) => {
+        const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+        const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
+        return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+      };
+
+      // Helper function to format a date to its year-week representation "YYYY-WW"
+      const formatWeek = (date) => {
+        return `${date.getFullYear()}-W${getWeekNumber(date)
+          .toString()
+          .padStart(2, "0")}`;
+      };
+
+      // Helper function to calculate unique days, weeks, and months
+      const calculateUniquePeriods = (transactions) => {
+        // Assuming you have transactions in your state or props
+        let tempUniqueDays = new Set(uniqueDays);
+        let tempUniqueWeeks = new Set(uniqueWeeks);
+        let tempUniqueMonths = new Set(uniqueMonths);
+
+        transactions.forEach((tx) => {
+          const date = new Date(tx.timeStamp * 1000);
+          tempUniqueDays.add(formatDate(date)); // Add to temporary unique days
+          tempUniqueWeeks.add(formatWeek(date)); // Add to temporary unique weeks
+          tempUniqueMonths.add(`${date.getFullYear()}-${date.getMonth() + 1}`); // Add to temporary unique months
+        });
+
+        // Update the state with the new Sets
+        setUniqueDays(tempUniqueDays);
+        setUniqueWeeks(tempUniqueWeeks);
+        setUniqueMonths(tempUniqueMonths);
+        setTotalUniqueDays(tempUniqueDays.size);
+        setTotalUniqueWeeks(tempUniqueWeeks.size);
+        setTotalUniqueMonths(tempUniqueMonths.size);
+      };
+
+      // Get the totals
+      calculateUniquePeriods(data.result);
     } catch (error) {
       console.error("Error fetching transactions:", error);
       return [];
@@ -98,45 +164,103 @@ function App() {
   };
 
   async function connectMetamask() {
+    // If we are currently requesting accounts, don't start another request
+    if (isRequestingAccounts) {
+      return;
+    }
+
+    // Indicate that we are starting a request
+    setIsRequestingAccounts(true);
+
     if (window.ethereum) {
-      const web3 = new Web3(window.ethereum);
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      // const accounts = await web3.eth.getAccounts();
-      setAccount(accounts[0]);
+      try {
+        const web3 = new Web3(window.ethereum);
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts[0]);
+        // const currentChainID = await web3.eth.getChainId().then(console.log);
+        // console.log(currentChainID);
+        const chainId = await window.ethereum.request({
+          method: "eth_chainId",
+        });
+        setNetwork(chainId.toString());
 
-      // const currentChainID = await web3.eth.getChainId().then(console.log);
-      // console.log(currentChainID);
-      const chainId = await window.ethereum.request({ method: "eth_chainId" });
-      setNetwork(chainId.toString());
+        const accountsChangedHandler = async (accounts) => {
+          console.log("Account changed: ", accounts[0]);
 
-      const accountsChangedHandler = async (accounts) => {
-        console.log("Account changed: ", accounts[0]);
+          if (accounts.length === 0) {
+            console.log("Please connect to MetaMask.");
+            // Optionally, prompt user to connect to MetaMask:
+            // await window.ethereum.request({ method: 'eth_requestAccounts' });
+          } else {
+            const newAccount = accounts[0];
+            setAccount(newAccount);
+          }
+        };
 
-        if (accounts.length === 0) {
-          console.log("Please connect to MetaMask.");
-          // Optionally, prompt user to connect to MetaMask:
-          // await window.ethereum.request({ method: 'eth_requestAccounts' });
-        } else {
-          const newAccount = accounts[0];
-          setAccount(newAccount);
-        }
-      };
+        window.ethereum.on("accountsChanged", accountsChangedHandler);
 
-      window.ethereum.on("accountsChanged", accountsChangedHandler);
+        const chainChangedHandler = (chainId) => {
+          window.location.reload();
+          console.log("Chain changed :", chainId);
+          setNetwork(chainId);
+        };
 
-      const chainChangedHandler = (chainId) => {
-        window.location.reload();
-        console.log("Chain changed :", chainId);
-        setNetwork(chainId);
-      };
-
-      window.ethereum.on("chainChanged", chainChangedHandler);
+        window.ethereum.on("chainChanged", chainChangedHandler);
+      } catch (error) {
+        // Handle errors properly
+        console.error("Error connecting to MetaMask:", error);
+      } finally {
+        // Indicate that the request has finished
+        setIsRequestingAccounts(false);
+      }
     } else {
-      alert("Please download Metamask");
+      alert("Please download MetaMask");
+      setIsRequestingAccounts(false);
     }
   }
+
+  // async function connectMetamask() {
+  //   if (window.ethereum) {
+  //     const web3 = new Web3(window.ethereum);
+  //     const accounts = await window.ethereum.request({
+  //       method: "eth_requestAccounts",
+  //     });
+  //     // const accounts = await web3.eth.getAccounts();
+  //     setAccount(accounts[0]);
+
+  //     // const currentChainID = await web3.eth.getChainId().then(console.log);
+  //     // console.log(currentChainID);
+  //     const chainId = await window.ethereum.request({ method: "eth_chainId" });
+  //     setNetwork(chainId.toString());
+
+  //     const accountsChangedHandler = async (accounts) => {
+  //       console.log("Account changed: ", accounts[0]);
+
+  //       if (accounts.length === 0) {
+  //         console.log("Please connect to MetaMask.");
+  //         // Optionally, prompt user to connect to MetaMask:
+  //         // await window.ethereum.request({ method: 'eth_requestAccounts' });
+  //       } else {
+  //         const newAccount = accounts[0];
+  //         setAccount(newAccount);
+  //       }
+  //     };
+
+  //     window.ethereum.on("accountsChanged", accountsChangedHandler);
+
+  //     const chainChangedHandler = (chainId) => {
+  //       window.location.reload();
+  //       console.log("Chain changed :", chainId);
+  //       setNetwork(chainId);
+  //     };
+
+  //     window.ethereum.on("chainChanged", chainChangedHandler);
+  //   } else {
+  //     alert("Please download Metamask");
+  //   }
+  // }
 
   async function switchNetwork() {
     const web3 = new Web3(window.ethereum);
@@ -251,10 +375,12 @@ function App() {
       <header className="App-header">
         Scroll Airdrop Checker
         <>
-          <button onClick={() => connectMetamask()}>
-            Connect to Metamask{" "}
+          <button onClick={connectMetamask} disabled={isRequestingAccounts}>
+            {isRequestingAccounts
+              ? "Waiting for MetaMask..."
+              : "Connect to MetaMask"}
           </button>
-          <button onClick={() => switchNetwork()}>Change to Scroll</button>
+          ;<button onClick={() => switchNetwork()}>Change to Scroll</button>
           <button onClick={() => getEtherBalance()}>Check balance</button>
           <button onClick={() => getTransactionsByAddress(account)}>
             Total transactions
@@ -295,6 +421,24 @@ function App() {
             {/* Render transactions list here */}
           </>
         )}
+        {isLoading ? (
+          <p>Loading transactions...</p>
+        ) : (
+          <>
+            <p>Succesful transactions: {numSuccesfulTransactions}</p>
+            {/* Render transactions list here */}
+          </>
+        )}
+        <ul>
+          Total unique:
+          <li>{totalUniqueDays ? `${totalUniqueDays} Days` : "Loading..."}</li>
+          <li>
+            {totalUniqueWeeks ? `${totalUniqueWeeks} Weeks` : "Loading..."}
+          </li>
+          <li>
+            {totalUniqueMonths ? `${totalUniqueMonths} Month` : "Loading..."}
+          </li>
+        </ul>
       </header>
     </div>
   );
